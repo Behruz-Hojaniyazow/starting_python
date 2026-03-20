7bc58e00c5cce3922899b1e3f4ce3edb0573d552
Behruz-Hojaniyazow <behruzhojaniyazow@gmail.com>
2026-03-20 21:21:11 +0500

    update 55_clever_students.py

--- 55_clever_students.py
+++ 55_clever_students.py
@@ -1,14 +1,20 @@
-try:
-	clever_students = 0
-	fallen = 0
-	for i in range(5):
-		score = float(input(f"How many points did student {i+1} get? "))
-		if score >= 90:
-			clever_students += 1
-		elif score < 90:
-			fallen += 1
-	print(f"{clever_students} student/s have/has managed to pass the exam with a high point")
-
-	print(f"{fallen} student/s have/has passed the exam with a low point")
-except ValueError:
-	print("please enter only numbers")
+clever_students = 0
+fallen = 0
+i = 1
+while i <= 5:
+  try:
+    score_input = input(f"How many points did student {i} get? ")
+    score = float(score_input)
+    if 0 <= score <= 100:
+      if score >= 90:
+        clever_students += 1
+      else:
+        fallen += 1
+      i += 1
+    else:
+      print("please enter a score between 0 and 100")
+  except ValueError:
+    print("Error, please enter only numbers")
+print("*" * 30)
+print(f"{clever_students} student/s passed with a high point")
+print(f"{fallen} student/s passed with a low point")
