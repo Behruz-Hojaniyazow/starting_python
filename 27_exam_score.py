5902b3d647d06eb9fc2c476f26ee450c146a4fcc
Behruz-Hojaniyazow <behruzhojaniyazow@gmail.com>
2026-03-22 14:13:03 +0500

    update 27_exam_score.py

--- 27_exam_score.py
+++ 27_exam_score.py
@@ -1,17 +1,28 @@
 students_scores = {}
-students_scores2 = []
 while True:
   print("Type 'Stop' to stop the project")
   student = input("Enter the student's name ").title()
-  score = float(input(f"enter {student}'s score: ")
-  students_scores[student] = score
-  students_scores2.append(score)
-  lowest_score = min(students_scores2)
-  highest_score = max(students_scores2)
-  average_score = sum(students_scores2) / len(students_scores2)
   if student == 'Stop':
     print("project was stopped")
     break
-for student, score in students_scores.items():
-  print(f"{student} got {score}")
-print(f"")
+  else:
+    while True:
+      try:
+        score = float(input(f"enter {student}'s score: "))
+  
+        students_scores[student] = score
+        break
+      except ValueError:
+        print("Please enter only numbers, Try again")
+if students_scores:
+  all_scores = list(students_scores.values())
+  highest_score = max(all_scores)
+  lowest_score = min(all_scores)
+  average_score = sum(all_scores) / len(all_scores)
+  for student, score in students_scores.items():
+   print(f"{student} got {score} scores")
+print(f"The highest score in class is {highest_score}")
+print(f"The lowest score in class is {lowest_score}")
+print(f"Average score in class is {average_score}")
+else:
+  print("Still no one has taken exam")
