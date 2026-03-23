54c093550ee0e6e278a91b76826a568cea7db1b5
Behruz-Hojaniyazow <behruzhojaniyazow@gmail.com>
2026-03-23 20:30:20 +0500

    update 62_perimeter_of_rectangle.py

---
+++ 62_perimeter_of_rectangle.py
@@ -0,0 +1,17 @@
+def get_number(string):
+  while True:
+    try:
+      number = float(input(string))
+      if number > 0:
+        return number
+      else:
+        print("Error, The number must ne positive, (greater than '0')")
+    except ValueError:
+      print("Please enter only numbers, Try again")
+def perimeter():
+  """ a function that finds the perimeter of rectangle"""
+  a = get_number("Enter the 'a': ")
+  b = get_number("Enter the 'b': ")
+  p = 2 * (a + b)
+  print(f"Perimeter of rectangle is equal to {p}")
+perimeter()
