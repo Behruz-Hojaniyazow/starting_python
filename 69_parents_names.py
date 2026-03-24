63b65405b2e059783e1ac4529d783193c0749d55
Behruz-Hojaniyazow <behruzhojaniyazow@gmail.com>
2026-03-24 21:42:22 +0500

    update 69_parents_names.py

---
+++ 69_parents_names.py
@@ -0,0 +1,5 @@
+def parents(father, mother):
+  """a function that greets your parents"""
+  print(f"Your father's name {father.title()}")
+  print(f"Your mother's name {mother.title()}")
+parents('anvar', 'zamira')
