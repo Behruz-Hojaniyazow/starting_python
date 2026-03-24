5520cda051e771a8561d8a6d7f323b7f12a9695e
Behruz-Hojaniyazow <behruzhojaniyazow@gmail.com>
2026-03-24 23:43:01 +0500

    update 70_solving_linear_eq.py

---
+++ 70_solving_linear_eq.py
@@ -0,0 +1,20 @@
+def get_number(prompt):
+  while True:
+    try:
+      return float(input(prompt))
+    except ValueError:
+      print("Please enter only numbers")
+def linear_eq():
+  """solving a linear equation in the form ax + b = 0"""
+  print("We solve the equation ax + b = 0")
+  a = get_number("Please enter the 'a': ")
+  b = get_number("Please enter the 'b': ")
+  if a == 0:
+    if b == 0:
+      print("The equation has infinite, many solutions.")
+    else:
+      print("The equation has no solution")
+  else:
+    x = -b / a
+  print(f"Solution to the equation: {x}")
+linear_eq()
